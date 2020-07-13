baa0cbe26aa8d3f3563c4953a598a0d84038b001
Red1 <redouanix@gmail.com>
2020-07-13 12:26:41 +0100

    update testMultiple url and multipleChoiceview view to enblobe all countries in the dataframe

--- HighchartsPlot/forms.py
+++ HighchartsPlot/forms.py
@@ -4,15 +4,13 @@ class CountriesForm(forms.Form):
     countries=forms.CharField(max_length=100)
 
 
+from .views import recovered
+choicesList=recovered.columns
 
+options=tuple(zip(choicesList,choicesList))
+# this is a a tuple of tuples of index and countrie peers
+# like this :
+# (('Afghanistan', 'Afghanistan'),('Albania', 'Albania'),('Algeria', 'Algeria'),('Andorra', 'Andorra'),.....)
 class myForm(forms.Form):
-    OPTIONS = (
-        ("AUT", "Austria"),
-        ("DEU", "Germany"),
-        ("NLD", "Neitherlands"),
-        ('MA','Morocco'),
-        ('TU','Tunisia'),
-        ('US','united States'),
-        ('Uk','United Kingdom')
-    )
-    choicesList = forms.MultipleChoiceField(choices=OPTIONS)
+    
+    choicesList = forms.MultipleChoiceField(choices=options)
